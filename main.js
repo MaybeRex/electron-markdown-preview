a010d868ad3b23881e4b1051b1ad0344e741f58c
Mario Solorzano <marioxcore1@gmail.com>
2017-01-15 22:24:34 -0800

    added basic funtionality

--- main.js
+++ main.js
@@ -8,7 +8,13 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow(
+    {
+      width: 1080,
+      height: 720,
+      titleBarStyle: 'hidden'
+    }
+  )
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,7 +24,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
